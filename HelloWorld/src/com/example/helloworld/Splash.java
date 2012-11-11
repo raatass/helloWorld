6a05fcdc6e460172668638e0429c298cd4f3a47e
Christian Wilhelmsen <christian.wilhelmsen87@gmail.com>
2012-11-11 22:47:16 +0100

    Changed manifest to open Splash

--- HelloWorld/src/com/example/helloworld/Splash.java
+++ HelloWorld/src/com/example/helloworld/Splash.java
@@ -1,5 +1,17 @@
 package com.example.helloworld;
 
-public class Splash {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class Splash extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		// Set splash.xml as content in the view
+		setContentView(R.layout.splash);
+	}
+	
 }
